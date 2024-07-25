5160c3b8cc6d50d84e9751df84e07ee33d100083
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-07-25 19:20:59 +0200

    Create nav bar

--- src/layouts/App.vue
+++ src/layouts/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import PWABadge from '@/modules/serviceworker/components/PWABadge.vue'
+import TheNavbar from '@/modules/bar/components/TheNavbar.vue'
 </script>
 
 <template>
+  <TheNavbar />
   <RouterView />
   <PWABadge />
 </template>
---
+++ src/modules/bar/components/TheNavbar.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav class="navbar">
+    <main class="navbar__content">
+      <router-link to="/">
+        <img src="@/assets/logos/app_logo.png" alt="Logo image" class="navbar__logo">
+      </router-link>
+    </main>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  .navbar {
+    box-shadow: 0 8px 12px rgba(#000, .1);
+    
+    &__content {
+      height: 60px;
+      padding: 0 1rem;
+      display: flex;
+      align-items: center;
+    }
+
+    &__logo {
+      width: 45px;
+    }
+  }
+</style>
