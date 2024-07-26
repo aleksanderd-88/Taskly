aae68538d8ae1678bad1861c3c3372d3b4425523
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-07-26 20:33:54 +0200

    Create function to set user details

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,19 +1,31 @@
 import { defineStore } from "pinia";
 import API from '@/services/api'
-import { CreateUserType } from "@/types/user";
+import { CreateUserType, UserType } from "@/types/user";
+import { ref } from "vue";
 
 export const useUserStore = defineStore('user', () => {
+
+  const currentUser = ref<UserType | null>(null)
 
   const createUser = async (params: CreateUserType) => {
     try {
       const { data } = await API.users.create(params)
+
+      setUser(data)
+
       return Promise.resolve(data)
     } catch (error) {
       return null
     }
   }
 
+  const setUser = (user: UserType) => {
+    currentUser.value = user
+    localStorage.setItem('__@taskly/user__', JSON.stringify(currentUser.value))
+  }
+
   return {
-    createUser
+    createUser,
+    setUser
   }
 })
