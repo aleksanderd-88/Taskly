15e7e9d7ca5bcf1de1c37c40dbaeab0359a5add0
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-08-02 14:15:11 +0200

    Implement dialog component

---
+++ src/common/components/AppDialog.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    default: false
+  },
+  headerTitle: {
+    type: String,
+    default: 'Edit Profile'
+  }
+})
+
+const emit = defineEmits<{
+  (event: 'close', value: boolean): void
+}>()
+
+const dialogIsVisible = computed({
+  get: () => props.isVisible,
+  set: (value: boolean) => emit('close', value)
+})
+</script>
+
+<template>
+  <PrimeDialog 
+    v-model:visible="dialogIsVisible" 
+    modal 
+    :header="headerTitle"
+  >
+    <slot />
+  </PrimeDialog>
+</template>
