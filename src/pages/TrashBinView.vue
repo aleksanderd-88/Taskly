dc9300ec4234861e6484e4689207f2b1ac9ef1ac
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-08-25 10:49:58 +0200

    Modify base layout

--- src/pages/TrashBinView.vue
+++ src/pages/TrashBinView.vue
@@ -9,49 +9,41 @@ const projects = computed(() => projectStore.result?.rows || [])
 </script>
 
 <template>
-  <div class="trash-bin">
-    <h1>Trash bin</h1>
+  <div class="base-layout">
+    <main class="base-layout__content">
+      <h1>Trash bin</h1>
 
-    <PButton
-      severity="secondary"
-      icon="pi pi-trash"
-      label="Empty trash"
-      :style="{ 
-          width: '100%', 
-          maxWidth: '150px', 
-          marginBottom: '1rem' 
-        }" 
-      />
-    
-    <DataTable
-      :value="projects"
-      tableStyle="min-width: 50rem"
-      size="small"
-      v-if="projects.length"
-    >
-      <DataColumn field="name" header="Name" style="width: 15%"></DataColumn>
-      <DataColumn field="deletedAt" header="Deleted at">
-        <template #body="slotProps">
-          {{ moment(new Date(slotProps.data.deletedAt)).format('YYYY-MM-DD HH:MM:ss') }}
-        </template>
-      </DataColumn>
+      <PButton
+        severity="secondary"
+        icon="pi pi-trash"
+        label="Empty trash"
+        :style="{ 
+            width: '100%', 
+            maxWidth: '150px', 
+            marginBottom: '1rem' 
+          }" 
+        />
       
-      <DataColumn>
-        <template #body>
-          <PButton icon="pi pi-undo" text severity="secondary" />
-          <PButton icon="pi pi-trash" text severity="secondary" />
-        </template>
-      </DataColumn>
-    </DataTable>
+      <DataTable
+        :value="projects"
+        tableStyle="min-width: 50rem"
+        size="small"
+        v-if="projects.length"
+      >
+        <DataColumn field="name" header="Name" style="width: 15%"></DataColumn>
+        <DataColumn field="deletedAt" header="Deleted at">
+          <template #body="slotProps">
+            {{ moment(new Date(slotProps.data.deletedAt)).format('YYYY-MM-DD HH:MM:ss') }}
+          </template>
+        </DataColumn>
+        
+        <DataColumn>
+          <template #body>
+            <PButton icon="pi pi-undo" text severity="secondary" />
+            <PButton icon="pi pi-trash" text severity="secondary" />
+          </template>
+        </DataColumn>
+      </DataTable>
+    </main>
   </div>
 </template>
-
-<style lang="scss" scoped>
-  .trash-bin {
-    padding: 1rem;
-
-    h1 {
-      margin-bottom: 1rem;
-    }
-  }
-</style>
--- src/pages/project/ProjectListView.vue
+++ src/pages/project/ProjectListView.vue
@@ -5,6 +5,8 @@ import TheProjectList from '@/modules/project/components/TheProjectList.vue';
 <template>
   <div class="base-layout">
     <main class="base-layout__content">
+      <h1>Projects</h1>
+      
       <TheProjectList />
     </main>
   </div>
