0e7f749695089edc4fd60fc2f823762bfa20fdcb
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-08-31 10:28:59 +0200

    Only show members that have been verified

--- api/src/controllers/project/list.ts
+++ api/src/controllers/project/list.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { RequestCustom } from "../../../types";
 import models from "../../../models";
 import get from 'lodash/get'
+import { FlattenMaps } from "mongoose";
 
 export default async (req: RequestCustom, res: Response) => {
   try {
@@ -13,12 +14,16 @@ export default async (req: RequestCustom, res: Response) => {
       data = { ...data, ...filter }
 
     let projects = await models.Project.find(data).lean()
+    let results: FlattenMaps<Record<string, unknown>[]> = []
     
     for (const project of projects) {
       project.tasks = await models.Task.find({ projectId: get(project, '_id', null) })
+      
+      const visibleMembers = project.members.filter(member => member.verified)
+      results = [ ...results, { ...project, ...{ members: visibleMembers } } ]
     }
 
-    res.status(200).send({ rows: projects, count: projects.length })
+    res.status(200).send({ rows: results, count: results.length })
   } catch (error) {
     res.status(500).send(get(error, 'message', 'Could not create project'))
   }
