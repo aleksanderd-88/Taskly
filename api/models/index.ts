f4e6cdad247f72e1a32cf4e0388a1a32ecb75cc4
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-09-04 00:18:31 +0200

    Move files

--- api/models/index.ts
+++
@@ -1,9 +0,0 @@
-import User from "./user"
-import Project from "./project"
-import Task from "./task"
-
-export default {
-  User,
-  Project,
-  Task
-}
--- api/src/controllers/health-check.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express"
-export default (req: Request, res: Response) => {
-  try {
-    res.status(200).send('API initialized')
-  } catch (error) {
-    res.status(500).send('API failed to initialize')
-  }
-}
